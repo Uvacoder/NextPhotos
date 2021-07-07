184a8f93cd8a39ee82a878c8a81bbcbba2f52dea
chiagozie Arinzechi <imakandiboi@yahoo.com>
2021-07-07 01:15:59 +0100

    commit message for project page

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <Header />
     </header>
     <main>
-      <router-view></router-view>
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </main>
 
     <footer>
@@ -28,4 +30,15 @@ export default {
 </script>
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.4s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
 </style>
---
+++ src/views/project.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Projectlander />
+  </div>
+</template>
+
+<script>
+import Projectlander from "@/components/Projectlander.vue";
+export default {
+  components: {
+    Projectlander,
+  },
+};
+</script>
+
+<style scoped>
+</style>
